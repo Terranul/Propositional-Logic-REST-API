061787d3ff2dcc16a7e289e15dbe6b43c1392030
Ben Faraone <bfaraone@students.cs.ubc.ca>
2026-07-01 17:26:12 -0700

    fixed the linear parses, just needs testing

--- Sources/ESTest/Services/EvaluationService/Parsing/IdeaParser.swift
+++ Sources/ESTest/Services/EvaluationService/Parsing/IdeaParser.swift
@@ -1,64 +1,126 @@
-// // an extrapolation of my idea for a faster parser
+// an extrapolation of my idea for a faster parser
 
-// class Stack<T> {
+fileprivate class DummyStatement {
+    var lhs: (any Statement)?
+    var rhs: (any Statement)?
+    var variable: Variable?
+    var op: (any Operator)?
+    var lop: [any LeadingOperator]
 
-//     var internalStack: Array<T>
+    // init for raw statement
+    init(variable: Variable, lop: any LeadingOperator) {
+        self.variable = variable
+        self.lop = [lop]
+    }
 
-//     init() {
-//         internalStack = []
-//     }
+    init() {
+        self.lop = []
+    }
 
-//     func push(_ value: T) {
-//         internalStack.append(value)
-//     }
+    func create() -> any Statement {
+        let resolvedLop: any LeadingOperator = lop.count == 0 ? DefaultLeadingOperator() : ComplexLeadingOperator(guts: lop)
+        if (rhs == nil && variable != nil) {
+            // we'll classify this as a raw statement
+            return RawStatement(variable: variable!, leadingOp: resolvedLop)
+        }
+        return LazyEvalComplexStatement(lhs: lhs!, rhs: rhs!, op: op!, leadingOp: resolvedLop)
+    }
 
-//     func pop() -> T? {
-//         return internalStack.removeLast()
-//     }
+    func createRaw(variable: Variable) -> RawStatement {
+        let resolvedLop: any LeadingOperator = lop.count == 0 ? DefaultLeadingOperator() : ComplexLeadingOperator(guts: lop)
+        return RawStatement(variable: variable, leadingOp: resolvedLop)
+    }
 
-//     func peek() -> T? {
-//         return internalStack.last
-//     }
-// }
+    func isComplete() -> Bool {
+        return lhs != nil && rhs != nil
+    }
 
-// let operatorParser = OperatorParser()
+    func isPremature() -> Bool {
+        return lop.count != 0
+    }
 
-// func linearParse(input: String) throws -> any Statement {
-//     let stack: Stack<any Statement> = Stack()
-//     for character in input {
-//         if (character == "(") {
-//             stack.push(getComplexStatement())
-//         } else if (character == ")") {
-//             if let newValue = stack.pop() {
-//                 fillStatement(stack.peek()! as! ComplexStatement, with: newValue)
-//             }
-//         } else if (operatorParser.isOperator(value: character)) {
-//             if let complx: ComplexStatement = stack.peek() as? ComplexStatement {
-//                 complx.op = try operatorParser.getOperator(value: character)
-//             } else {
-//                 // error thrown
-//             }
-//         } else if (operatorParser.isLeadingOperator(value: character)) {
-//             stack.peek()!.setLop(lop: operatorParser.getLeadingOperator(value: character))
-//         } else {
-//             // we have a variable 
-//             fillStatement(stack.peek()! as! ComplexStatement, with: RawStatement(variable: character, leadingOp: DefaultLeadingOperator()))
-//         }
-//     }
-// }
+    func fill(with value: any Statement) {
+        if (lhs != nil) {
+            lhs = value
+        } else {
+            rhs = value
+        }
+    }   
+}
 
-// fileprivate func getComplexStatement() -> ComplexStatement {
-//     let dummyRawStatement: RawStatement = RawStatement(variable: "v", leadingOp: DefaultLeadingOperator())
-//     return ComplexStatement(lhs: dummyRawStatement, rhs: dummyRawStatement, op: AndOperator(), leadingOp: DefaultLeadingOperator())
-// }
+class Stack<T> {
 
-// fileprivate func fillStatement(_ value: ComplexStatement, with fillee: any Statement) {
-//     // we will use the variable being "v" to mark that the position has yet to be filled
-//     // a "v" variable is impossible to be acheived as it is a operator keyword
-//     // this will need to be modified later when we add operator mapping
-//     if (value.lhs.getVariables()[0] == "v") {
-//         value.lhs = fillee
-//     } else {
-//         value.rhs = fillee
-//     }
-// }   
+    var internalStack: Array<T>
+
+    init() {
+        internalStack = []
+    }
+
+    func push(_ value: T) {
+        internalStack.append(value)
+    }
+
+    func pop() -> T? {
+        return internalStack.removeLast()
+    }
+
+    func peek() -> T? {
+        return internalStack.last
+    }
+}
+
+let operatorParser = OperatorParser()
+
+func linearParse(input: String) throws -> any Statement {
+    let stack: Stack<DummyStatement> = Stack()
+    for character in input {
+        if (character == "(") {
+            if (stack.peek() != nil) {
+                if (!stack.peek()!.isPremature()) {
+                    stack.push(DummyStatement())
+                }
+            } else {
+                stack.push(DummyStatement())
+            }
+        } else if (character == ")") {
+            if let newStatement = stack.pop()?.create() {
+                if let fillStatement = stack.peek() {
+                    fillStatement.fill(with: newStatement)
+                } else {
+                    return newStatement
+                }
+            }
+        } else if (operatorParser.isOperator(value: character)) {
+            if let curStatement = stack.peek() {
+                curStatement.op = try operatorParser.getOperator(value: character)
+            } else {
+                // throw error
+            }
+        } else if (operatorParser.isLeadingOperator(value: character)) {
+            // prematurely throw onto the stack
+            if (stack.peek() != nil && stack.peek()!.isPremature()) {
+                // case where we have chained lops
+                stack.peek()!.lop.append(operatorParser.getLeadingOperator(value: character))
+            } else {
+                let newStatement = DummyStatement()
+                newStatement.lop = [operatorParser.getLeadingOperator(value: character)]
+                stack.push(newStatement)
+            }
+        } else {
+            // variable
+            var newRaw: RawStatement? = nil
+            if (stack.peek() != nil && stack.peek()!.isPremature()) {
+                newRaw = stack.pop()!.createRaw(variable: character)
+            } else {
+                newRaw = RawStatement(variable: character, leadingOp: DefaultLeadingOperator())
+            }
+            if let curStatement = stack.peek() {
+                curStatement.fill(with: newRaw!)
+            } else {
+                return newRaw!
+            }
+        }
+    }
+    // should never hit this point
+    throw EvalError.InternalParsingError
+}
